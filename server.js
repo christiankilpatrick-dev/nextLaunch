a3e3f81398f0f332ad4422d2610dcb83fa4fd067
Christian Kilpatrick <cckilpat@gmail.com>
2023-03-11 14:23:23 -0500

    testing

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const methodOverride = require('method-override');
-const flash = require('express-flash');
-const logger = require('morgan');
-const connectDB = require('./config/database');
-const mainRoutes = require('./routes/main');
-const postRoutes = require('./routes/posts');
-const commentRoutes = require('./routes/comments');
-const moment = require('moment');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+const moment = require("moment");
 
 app.use((req, res, next) => {
-	res.locals.moment = moment;
-	next();
+  res.locals.moment = moment;
+  next();
 });
 //Use .env file in config folder
-require('dotenv').config({ path: './config/.env' });
+require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Connect To Database
 connectDB();
 
 //Using EJS for views
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Static Folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Logging
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 //Use forms for put / delete
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB
 app.use(
-	session({
-		secret: 'keyboard cat',
-		resave: false,
-		saveUninitialized: false,
-		store: new MongoStore({ mongooseConnection: mongoose.connection }),
-	})
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
 );
 
 // Passport middleware
@@ -60,11 +60,11 @@ app.use(passport.session());
 app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
-app.use('/', mainRoutes);
-app.use('/post', postRoutes);
-app.use('/comment', commentRoutes);
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-	console.log('Server is running, you better catch it!');
+  console.log("Server is running, you better catch it!");
 });
